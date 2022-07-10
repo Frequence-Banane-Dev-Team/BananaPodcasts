8291cd2fb61f9fefa4b66e4430766c57b9ae6e24
Alexandre Tellier <alexandre@MacBook-Pro-de-Alexandre.local>
2022-07-10 18:16:35 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+def myconfig(app):
+   app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_DATABASE_USER']
+   app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_DATABASE_PASSWORD']
+   app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE_DB']
+   app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_DATABASE_HOST']
+   app.config['MYSQL_DATABASE_PORT'] = int(os.environ['MYSQL_DATABASE_PORT'])
+   app.secret_key = os.environ['APP_SECRET_KEY'].encode()
